06e20445b7fe365c9a5dabf3c11d233a870eaa18
wenlongfei <wennersoft@sina.com>
2014-03-21 17:47:27 +0800

    登录控制器修改

--- src/com/gmteam/framework/component/login/web/LoginControll.java
+++ src/com/gmteam/framework/component/login/web/LoginControll.java
@@ -1,32 +1,21 @@
 package com.gmteam.framework.component.login.web;
 
-import java.io.Serializable;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.gmteam.framework.component.login.pojo.PlatUser;
+
 @Controller
-public class LoginControll implements Serializable {
-    @RequestMapping(value="/login.do",method=RequestMethod.POST)
-    public @ResponseBody Json Login(HttpServletRequest request,HttpServletResponse response) {
-        Json j = new Json();
-        String name = request.getParameter("name");
-        System.out.println(name);
-        String password = request.getParameter("password");
-        System.out.println(password);
-        if("zhangsan".equals(name)&&"aaa".equals(password)){
-            System.out.println("进入success");
-            j.setMessage("登录成功！欢迎您"+name);
-            j.setSuccess(true);
-        }else{
-            j.setMessage("失败，用户名密码错误");
-            j.setSuccess(false);
-        }
-        return j;
+public class LoginControll {
+    @RequestMapping(value="/login.do")
+    public @ResponseBody String Login(PlatUser user) {
+    	System.out.println("进入l losgin Contsdsoller");
+    	System.out.println(user.getLoginName());
+    	System.out.println(user.getPassword());
+		return null;
+    	
+     
+    	
     }
 }
