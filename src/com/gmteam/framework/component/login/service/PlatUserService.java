809f021244b4913e3a0fabb36ff941ebb9f9816e
wenlongfei <wennersoft@sina.com>
2014-03-24 16:30:25 +0800

    登录模块0.1

---
+++ src/com/gmteam/framework/component/login/service/PlatUserService.java
@@ -0,0 +1,30 @@
+package com.gmteam.framework.component.login.service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.gmteam.framework.component.login.pojo.PlatUser;
+import com.gmteam.framework.core.dao.mybatis.MybatisDAO;
+
+@Service
+public class PlatUserService {
+    @Resource(name="defaultDAO")
+	private MybatisDAO<PlatUser> dao;
+
+	@PostConstruct
+	public void initParam() {
+	dao.setNamespace("Plat_User");
+	}
+	
+	public PlatUser getPlatUserByLoginName(String loginName){
+	    try {
+            return dao.getInfoObject("selectPlatUserByLoginName", loginName);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	    return null;
+	}
+}
--- src/com/gmteam/framework/component/login/web/LoginControll.java
+++ src/com/gmteam/framework/component/login/web/LoginControll.java
@@ -1,18 +1,35 @@
 package com.gmteam.framework.component.login.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.gmteam.framework.component.login.pojo.PlatUser;
 import com.gmteam.framework.component.login.pojo.UserLogin;
+import com.gmteam.framework.component.login.service.PlatUserService;
 
 @Controller
 public class LoginControll {
-    @RequestMapping(value="/login.do")
+	
+    private PlatUserService userService;
+    
+    public PlatUserService getUserService() {
+        return userService;
+    }
+    @Autowired
+    public void setUserService(PlatUserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/login.do")
     public @ResponseBody String Login(UserLogin user) {
     	System.out.println("进入l losgin Contsdsoller");
     	System.out.println(user.getLoginName());
     	System.out.println(user.getPassword());
+    	PlatUser u =userService.getPlatUserByLoginName(user.getLoginName());
+    	System.out.println(u.getLoginName());
+        System.out.println(u.getPassword());
 		return null;
     }
 }
