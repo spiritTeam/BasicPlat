142adc8db7b3a119ef57b4da63a5615300a865cd
wenlongfei <wennersoft@sina.com>
2014-03-25 15:43:33 +0800

    登录控制器的第一个版本0.1(基本登录可以实现)

--- src/com/gmteam/framework/component/login/web/LoginController.java
+++ src/com/gmteam/framework/component/login/web/LoginController.java
@@ -3,6 +3,7 @@ package com.gmteam.framework.component.login.web;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gmteam.framework.IConstants;
+import com.gmteam.framework.cache.CacheEle;
 import com.gmteam.framework.cache.SystemCache;
 import com.gmteam.framework.component.login.pojo.PlatUser;
 import com.gmteam.framework.component.login.pojo.UserLogin;
@@ -19,45 +21,40 @@ import com.gmteam.framework.component.login.service.PlatUserService;
 
 @Controller
 public class LoginController {
-	
+	@Resource
     private PlatUserService userService;
     
-    public PlatUserService getUserService() {
-        return userService;
-    }
-    @Autowired
-    public void setUserService(PlatUserService userService) {
-        this.userService = userService;
-    }
-
     /**
      * @param outSideUser
      * @param req
      * @return
      */
     @RequestMapping("/login.do")
-    public @ResponseBody Map<String,Object> Login(PlatUser outSideUser,HttpServletRequest req) {
+    public @ResponseBody Map<String,Object> Login(UserLogin userLogin,HttpServletRequest req) {
         Map<String,Object> retObj = new HashMap<String,Object>();
-    	PlatUser user;
         try {
-            user = userService.getPlatUserByLoginName(outSideUser.getLoginName());
+            PlatUser user = userService.getPlatUserByLoginName(userLogin.getLoginName());
             if(user==null){
                 retObj.put("type", "2");
-                retObj.put("data", "没有登录名为["+outSideUser.getLoginName()+"]的用户！");
-            }else if(!outSideUser.getPassword().equals(user.getPassword())){
+                retObj.put("data", "没有登录名为["+userLogin.getLoginName()+"]的用户！");
+            }else if(!userLogin.getPassword().equals(user.getPassword())){
                 retObj.put("type", "2");
                 retObj.put("data", "密码不匹配！");
             }else{
                 //设置用户Session缓存
                 HttpSession session = req.getSession();
-                Map<String,UserLogin> userSessionMap = (Map<String, UserLogin>) SystemCache.getCache(IConstants.USERSESSIONMAP);
-                UserLogin userLogin = ((Map<String, UserLogin>)SystemCache.getCache(IConstants.USERSESSIONMAP)).remove(user.getId());
+                if(SystemCache.getCache(IConstants.USERSESSIONMAP)==null){
+                    SystemCache.setCache(
+                            new CacheEle<Map<String, UserLogin>>(IConstants.USERSESSIONMAP, "用户Session缓存", new HashMap<String, UserLogin>())
+                        );
+                }
+                UserLogin oldUserLogin = ((CacheEle<Map<String, UserLogin>>) SystemCache.getCache(IConstants.USERSESSIONMAP)).getContent().remove(user.getId());
                 userLogin.setSessionId(session.getId());
-                ((Map<String,UserLogin>)SystemCache.getCache(IConstants.USERSESSIONMAP)).put(user.getId(), userLogin);
+                ((CacheEle<Map<String, UserLogin>>) SystemCache.getCache(IConstants.USERSESSIONMAP)).getContent().put(user.getId(), userLogin);
                 //写用户信息
                 session.setAttribute(IConstants.SESSION_USER, user);
                 retObj.put("type", "1");
-                retObj.put("data", userLogin);
+                retObj.put("data", oldUserLogin);
             }
         } catch (Exception e) {
             e.printStackTrace();
