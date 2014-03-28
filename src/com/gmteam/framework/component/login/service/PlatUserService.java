f10e8b459cf6eeef82565e2ef72e2e30cf19873c
wenlongfei <wennersoft@sina.com>
2014-03-28 16:57:08 +0800

    调整代码（去tab）

--- src/com/gmteam/framework/component/login/service/PlatUserService.java
+++ src/com/gmteam/framework/component/login/service/PlatUserService.java
@@ -2,26 +2,22 @@ package com.gmteam.framework.component.login.service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-
 import org.springframework.stereotype.Service;
-
 import com.gmteam.framework.component.login.pojo.PlatUser;
 import com.gmteam.framework.core.dao.mybatis.MybatisDAO;
-
 @Service
 public class PlatUserService {
     @Resource(name="defaultDAO")
-	private MybatisDAO<PlatUser> dao;
+    private MybatisDAO<PlatUser> dao;
 
-	@PostConstruct
-	public void initParam() {
-	    dao.setNamespace("Plat_User");
+    @PostConstruct
+    public void initParam() {
+        dao.setNamespace("Plat_User");
 	}
-	
-	public PlatUser getPlatUserByLoginName(String loginName) throws Exception{
-           return dao.getInfoObject("selectPlatUserByLoginName", loginName);
-	}
-	public void addPlatUser(PlatUser user) throws Exception{
-	    dao.insert(user);
-	}
+    public PlatUser getPlatUserByLoginName(String loginName) throws Exception{
+        return dao.getInfoObject("selectPlatUserByLoginName", loginName);
+    }
+    public void addPlatUser(PlatUser user) throws Exception{
+        dao.insert(user);
+    }
 }
