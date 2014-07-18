2a2b754644ddcb8c47b79cacf1f02fc06fce74b5
wanghui <wanghui@nasoft.com>
2014-07-18 10:21:58 +0800

    UGA的Authority

--- src/com/gmteam/framework/UGA/UgaConstants.java
+++ src/com/gmteam/framework/UGA/UgaConstants.java
@@ -4,4 +4,5 @@ public class UgaConstants {
     //-缓存常量------------------------------------------------------------------------
     public final static String CATCH_UGA_USER="UgaUserInMemory";//UGA用户缓存
     public final static String CATCH_UGA_MODULE="UgaModuleInMemory";//UGA模块缓存
+    public final static String CATCH_UGA_USERMODULE="UgaUserModuleInMemory";//UGA用户模块关系缓存
 }
--- src/com/gmteam/framework/UGA/UgaUserService.java
+++ src/com/gmteam/framework/UGA/UgaUserService.java
@@ -2,12 +2,11 @@ package com.gmteam.framework.UGA;
 
 /**
  * UGA框架。
- * 用户服务对象的基础类，用户服务需继承此类。
+ * 用户服务的基础类，用户服务需继承此类。
  * 否则本框架登录会出现问题。
  * @author wanghui
  */
 public interface UgaUserService {
-
     /**
      * 根据登录名，得到用户信息
      * @param loginName 用户登录名
