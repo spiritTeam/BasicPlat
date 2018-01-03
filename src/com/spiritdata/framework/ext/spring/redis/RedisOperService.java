101344ef22310d0b2e0dcbbf4974a73c8b06ba1d
wanghuiwhwx <wanghui_whwx@sina.com>
2018-01-03 12:36:03 +0800

    test
    Signed-off-by: 19980418wx

--- src/com/spiritdata/framework/ext/spring/redis/RedisOperService.java
+++ src/com/spiritdata/framework/ext/spring/redis/RedisOperService.java
@@ -1,19 +1,17 @@
 package com.spiritdata.framework.ext.spring.redis;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.data.redis.connection.RedisStringCommands.SetOption;
+import org.springframework.data.redis.connection.jedis.JedisClusterConnection;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.util.Assert;
 
-//import java.util.HashMap;
-//import java.util.Map;
-//import com.spiritdata.framework.core.lock.BlockLockConfig;
-//import com.spiritdata.framework.core.lock.ExpirableBlockKey;
-//import com.spiritdata.framework.ext.redis.lock.RedisBlockLock;
-//import com.spiritdata.framework.util.JsonUtils;
-//import com.spiritdata.framework.ext.redis.GetBizData;
-
 import redis.clients.jedis.Jedis;
+import redis.clients.util.SafeEncoder;
 
 /**
  * 在Spring的Redis基础之上包的一层Redis操作方法服务类。
@@ -27,6 +25,8 @@ import redis.clients.jedis.Jedis;
  */
 public class RedisOperService {
     private Jedis jedis=null;
+    private JedisClusterConnection jedisCluster=null;
+    private int __type=-1;//=0redis直接链接;//=1redis集群链接;//=-1不链接
 
     /**
      * 构造函数，必须要设置连接
@@ -36,8 +36,14 @@ public class RedisOperService {
     public RedisOperService(JedisConnectionFactory conn) {
         super();
         Assert.notNull(conn, "请设置Spring-Redis的连接类");
-        this.jedis=conn.getShardInfo().createResource();
-        this.jedis.select(conn.getDatabase());
+        try {
+            this.jedisCluster=(JedisClusterConnection)conn.getClusterConnection();
+            this.__type=1;
+        } catch(InvalidDataAccessApiUsageException e) {
+            this.jedis=conn.getShardInfo().createResource();
+            this.jedis.select(conn.getDatabase());
+            this.__type=0;
+        }
     }
     /**
      * 构造函数，必须要设置连接
@@ -46,14 +52,47 @@ public class RedisOperService {
     public RedisOperService(JedisConnectionFactory conn, int dbIndex) {
         super();
         Assert.notNull(conn, "请设置Spring-Redis的连接类");
-        this.jedis=conn.getShardInfo().createResource();
-        this.jedis.select(dbIndex);//默认用第0个数据库
+        try {
+            this.jedisCluster=(JedisClusterConnection)conn.getClusterConnection();
+            this.__type=1;
+        } catch(InvalidDataAccessApiUsageException e) {
+            this.jedis=conn.getShardInfo().createResource();
+            this.jedis.select(dbIndex);//默认用第0个数据库
+            this.__type=0;
+        }
+    }
+
+    /**
+     * 构造函数，必须要设置连接
+     * @param conn
+     */
+    public RedisOperService(JedisConnectionFactory conn, int dbIndex, int type) {
+        super();
+        Assert.notNull(conn, "请设置Spring-Redis的连接类");
+        if (type==1) {
+            this.jedisCluster=(JedisClusterConnection)conn.getClusterConnection();
+            this.__type=1;
+        } if (type==0) {
+            this.jedis=conn.getShardInfo().createResource();
+            this.jedis.select(dbIndex);//默认用第0个数据库
+            this.__type=0;
+        } else {
+            try {
+                this.jedisCluster=(JedisClusterConnection)conn.getClusterConnection();
+                this.__type=1;
+            } catch(InvalidDataAccessApiUsageException e) {
+                this.jedis=conn.getShardInfo().createResource();
+                this.jedis.select(dbIndex);//默认用第0个数据库
+                this.__type=0;
+            }
+        }
     }
     /**
      * 关闭此连接
      */
     public void close() {
-        if (jedis!=null) jedis.close();
+        if (__type==0&&jedis!=null) jedis.close();
+        if (__type==1&&jedisCluster!=null) jedisCluster.close();
     }
 
     /**
@@ -62,7 +101,29 @@ public class RedisOperService {
      * @param value 数据的值
      */
     public boolean set(String key, String value) {
-        return (jedis.set(key, value)).equals("OK");
+        if (__type==0) return (jedis.set(key, value)).equals("OK");
+        else {
+            jedisCluster.set(key.getBytes(), value.getBytes());
+            return true;
+        }
+    }
+
+    /**
+     * 设置数据
+     * @param key 数据的key
+     * @param value 数据的值
+     * @param expireTime 过期时间，只支持毫秒，若是-1则表示没有过期时间
+     * @param optin 操作 只支持NX,XX其他都按无处理
+     */
+    public boolean set(String key, String value, long expireTime) {
+        if (__type==0) {
+            String _sta=jedis.set(key, value);
+            if (!_sta.equals("OK")) return false;
+        } else
+        if (__type==1) {
+            jedisCluster.set(key.getBytes(), value.getBytes());
+        } else return false;
+        return this.pExpire(key, expireTime);
     }
 
     /**
@@ -77,27 +138,28 @@ public class RedisOperService {
         String _option=(option==null?"":(option.equals("NX")||option.equals("XX")?option:"")); //处理操作
 
         if (_option.equals("")) {
-            if (expireTime>0) return set(key, value, expireTime);
-            else return set(key, value);
+            if (expireTime>0) {
+                return set(key, value, expireTime);
+            } else {
+                return set(key, value);
+            }
         } else {
-            if (expireTime>0) _sta=jedis.set(key, value, _option, "PX", expireTime);
-            else _sta=jedis.set(key, value, _option);
-            if (_sta==null) return false;
-            return _sta.equals("OK");
+            if (__type==0) {
+                if (expireTime>0) _sta=jedis.set(key, value, _option, "PX", expireTime);
+                else _sta=jedis.set(key, value, _option);
+                if (_sta==null) return false;
+                return _sta.equals("OK");
+            } else
+            if (__type==1) {
+                SetOption _so=SetOption.UPSERT;
+                if (option.equals("NX")) _so=SetOption.SET_IF_ABSENT;
+                if (option.equals("XX")) _so=SetOption.SET_IF_PRESENT;
+                Expiration expire=Expiration.milliseconds(expireTime);
+                if (expireTime<=0) expire=Expiration.persistent();
+                jedisCluster.set(key.getBytes(), value.getBytes(), expire, _so);
+                return true;
+            } else return false;
         }
-    }
-
-    /**
-     * 设置数据
-     * @param key 数据的key
-     * @param value 数据的值
-     * @param expireTime 过期时间，只支持毫秒，若是-1则表示没有过期时间
-     * @param optin 操作 只支持NX,XX其他都按无处理
-     */
-    public boolean set(String key, String value, long expireTime) {
-        String _sta=jedis.set(key, value);
-        if (!_sta.equals("OK")) return false;
-        return this.pExpire(key, expireTime);
     }
 
     /**
@@ -108,7 +170,14 @@ public class RedisOperService {
      * @param optin 操作 只支持NX,XX其他都按无处理
      */
     public String get(String key) {
-        return jedis.get(key);
+        if (__type==0) return jedis.get(key);
+        else
+        if (__type==1) {
+            byte[] ba=jedisCluster.get(key.getBytes());
+            if (ba==null) return null;
+            return new String(ba);
+        }
+        return null;
     }
 
     /**
@@ -117,7 +186,16 @@ public class RedisOperService {
      * @return 返回在参数key序列中，存在的key的个数
      */
     public long exists(String... keys) {
-        return jedis.exists(keys);
+        if (__type==0) return jedis.exists(keys);
+        else
+        if (__type==1) {
+            long count=0;
+            for (String k: keys) {
+                if (jedisCluster.exists(k.getBytes())) count++;
+            }
+            return count;
+        }
+        return 0;
     }
 
     /**
@@ -126,7 +204,10 @@ public class RedisOperService {
      * @return 若存在返回true，否则返回false
      */
     public boolean exist(String key) {
-        return jedis.exists(key);
+        if (__type==0) return jedis.exists(key);
+        else
+        if (__type==1) return jedisCluster.exists(key.getBytes());
+        return false;
     }
     
     /**
@@ -136,7 +217,10 @@ public class RedisOperService {
      * @return 设置成功返回true，设置失败(key不存在，或[在redis2.1.3版本之前]已经有过期时间)返回false
      */
     public boolean pExpire(String key, long expireTime) {
-        return jedis.pexpire(key, expireTime)==1;
+        if (__type==0) return jedis.pexpire(key, expireTime)==1;
+        else
+        if (__type==1) return jedisCluster.pExpire(key.getBytes(), expireTime);
+        return false;
     }
 
     /**
@@ -145,7 +229,9 @@ public class RedisOperService {
      * @param values 插入数据后，数组的长度
      */
     public void rPush(String key, String... values) {
-        jedis.rpush(key, values);
+        if (__type==0) jedis.rpush(key, values);
+        else
+        if (__type==1) jedisCluster.rPush(key.getBytes(), SafeEncoder.encodeMany(values));
     }
 
     /**
@@ -156,7 +242,16 @@ public class RedisOperService {
      * @return 子数组，以String为元素
      */
     public List<String> lRange(String key, long start, long end) {
-        return jedis.lrange(key, start, end);
+        if (__type==0) return jedis.lrange(key, start, end);
+        else
+        if (__type==1) {
+            List<byte[]> lr=jedisCluster.lRange(key.getBytes(), start, end);
+            if (lr==null||lr.isEmpty()) return null;
+            List<String> r=new ArrayList<String>();
+            for (byte[] ba: lr) r.add(new String(ba));
+            return r;
+        }
+        return null;
     }
 
     /**
@@ -165,7 +260,10 @@ public class RedisOperService {
      * @return 数组的长度
      */
     public Long lLen(String key) {
-        return jedis.llen(key);
+        if (__type==0) return jedis.llen(key);
+        else
+        if (__type==1) return jedisCluster.lLen(key.getBytes());
+        return 0l;
     }
 
     /**
@@ -175,7 +273,10 @@ public class RedisOperService {
      * @return 删除掉的元素的个数
      */
     public Long lRemove(String key, String value) {
-        return jedis.lrem(key, 0, value);
+        if (__type==0) return jedis.lrem(key, 0, value);
+        else
+        if (__type==1) return jedisCluster.lRem(key.getBytes(), 0, value.getBytes());
+        return 0l;
     }
 
     /**
@@ -184,45 +285,21 @@ public class RedisOperService {
      * @return 成功删除的key值数
      */
     public long del(String... keys) {
-        return jedis.del(keys);
+        if (__type==0) return jedis.del(keys);
+        else
+        if (__type==1) {
+            jedisCluster.del(SafeEncoder.encodeMany(keys));
+            return keys.length;
+        }
+        return 0;
     }
 
     /**
      * 清除该库下的所有key
      */
     public void cleanDB() {
-        jedis.flushDB();
+        if (__type==0) jedis.flushDB();
+        else
+        if (__type==1) jedisCluster.flushDb();
     }
-//
-//    /**
-//     * 获得并设置Key值
-//     * @param key key值
-//     * @param getData 获得业务数据的类
-//     * @param expiredTime 过期时间
-//     * @param isResident 如果Redis中有数据，是否钉在内存中，若是false，则该条数据将按照过期时间失效
-//     * @return 所获得的值
-//     */
-//    public Map<String, Object> getAndSet(String key, RedisGetAndSet getData, long expiredTime, boolean isResident) {
-//        ExpirableBlockKey rLock=RedisBlockLock.lock(key+"=LOCK", this, new BlockLockConfig(500, 2, 0, 60*1000));
-//        Map<String, Object> ret=new HashMap<String, Object>();
-//        try {
-//            Object _ret=jedis.get(key);
-//            if (_ret==null) {
-//                _ret=getData.getBizData();
-//                if (_ret!=null) {
-//                    set(key, JsonUtils.objToJson(_ret), expiredTime);
-//                    ret.put("FromBiz", _ret);
-//                }
-//            } else {
-//                if (isResident) pExpire(key, expiredTime);
-//                ret.put("FromRedis", _ret);
-//            }
-//            return ret.size()==0?null:ret;
-//        } catch(Exception e) {
-//            e.printStackTrace();
-//            return null;
-//        } finally {
-//            rLock.unlock();
-//        }
-//    }
 }
